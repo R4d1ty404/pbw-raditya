3fd05635f59757800534c68c34602bcf6f846d75
Raditya Perwira Putra <raditpp315@gmail.com>
2026-01-08 17:36:24 +0700

    Update sql dump

--- webdailyjournal.sql
+++ webdailyjournal.sql
@@ -1,19 +1,20 @@
--- MySQL dump 10.13  Distrib 8.0.44, for Linux (x86_64)
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19-11.8.3-MariaDB, for debian-linux-gnu (x86_64)
 --
 -- Host: localhost    Database: webdailyjournal
 -- ------------------------------------------------------
--- Server version	8.0.44-0ubuntu0.22.04.2
+-- Server version	11.8.3-MariaDB-0+deb13u1 from Debian
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8mb4 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
 
 --
 -- Table structure for table `article`
@@ -21,16 +22,16 @@
 
 DROP TABLE IF EXISTS `article`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8mb4 */;
 CREATE TABLE `article` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `judul` text COLLATE utf8mb4_general_ci,
-  `isi` text COLLATE utf8mb4_general_ci,
-  `gambar` text COLLATE utf8mb4_general_ci,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `judul` text DEFAULT NULL,
+  `isi` text DEFAULT NULL,
+  `gambar` text DEFAULT NULL,
   `tanggal` datetime DEFAULT NULL,
-  `username` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `username` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -39,9 +40,51 @@ CREATE TABLE `article` (
 
 LOCK TABLES `article` WRITE;
 /*!40000 ALTER TABLE `article` DISABLE KEYS */;
-INSERT INTO `article` VALUES (1,'Perpustakaan Kampus','This is a wider card with supporting text','1.jpg','2023-11-08 04:43:12','admin'),(2,'Ruang Kelas','This is a wider card with supporting text','2.jpg','2023-11-08 04:44:12','admin'),(3,'Kelompok Belajar','This is a wider card with supporting text','3.jpg','2023-11-08 04:45:12','admin'),(4,'Auditorium','This is a wider card with supporting text','4.jpg','2023-11-08 04:46:12','admin'),(22,'Taman','This is a wider card with supporting text','5.jpg','2023-11-08 04:47:12','admin'),(29,'Test','afwafw','20251220083835.png','2025-12-24 10:38:49','admin');
+set autocommit=0;
+INSERT INTO `article` VALUES
+(1,'Perpustakaan Kampus','This is a wider card with supporting text','1.jpg','2023-11-08 04:43:12','admin'),
+(2,'Ruang Kelas','This is a wider card with supporting text','2.jpg','2023-11-08 04:44:12','admin'),
+(3,'Kelompok Belajar','This is a wider card with supporting text','3.jpg','2023-11-08 04:45:12','admin'),
+(4,'Auditorium','This is a wider card with supporting text','4.jpg','2023-11-08 04:46:12','admin'),
+(22,'Taman','This is a wider card with supporting text','5.jpg','2023-11-08 04:47:12','admin'),
+(29,'Test','Testing','20251220083835.png','2026-01-08 14:19:44','admin');
 /*!40000 ALTER TABLE `article` ENABLE KEYS */;
 UNLOCK TABLES;
+commit;
+
+--
+-- Table structure for table `gallery`
+--
+
+DROP TABLE IF EXISTS `gallery`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `gallery` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gambar` text DEFAULT NULL,
+  `tanggal` datetime DEFAULT current_timestamp(),
+  `username` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `gallery`
+--
+
+LOCK TABLES `gallery` WRITE;
+/*!40000 ALTER TABLE `gallery` DISABLE KEYS */;
+set autocommit=0;
+INSERT INTO `gallery` VALUES
+(1,'20260108170552.jpg','2026-01-08 17:05:52','admin'),
+(2,'20260108170601.jpg','2026-01-08 17:06:01','admin'),
+(3,'20260108170607.jpg','2026-01-08 17:06:07','admin'),
+(4,'20260108170612.jpg','2026-01-08 17:06:12','admin'),
+(5,'20260108170619.jpg','2026-01-08 17:06:19','admin'),
+(6,'20260108170625.png','2026-01-08 17:06:25','admin');
+/*!40000 ALTER TABLE `gallery` ENABLE KEYS */;
+UNLOCK TABLES;
+commit;
 
 --
 -- Table structure for table `user`
@@ -49,13 +92,14 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user` (
-  `id` int DEFAULT NULL,
-  `username` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `password` text COLLATE utf8mb4_general_ci,
-  `foto` text COLLATE utf8mb4_general_ci
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) DEFAULT NULL,
+  `password` text DEFAULT NULL,
+  `foto` text DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,9 +108,13 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (NULL,'admin','e10adc3949ba59abbe56e057f20f883e','');
+set autocommit=0;
+INSERT INTO `user` VALUES
+(1,'admin','e10adc3949ba59abbe56e057f20f883e',''),
+(7,'danny','c3284d0f94606de1fd2af172aba15bf3','20260108154511.png');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
+commit;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -75,6 +123,6 @@ UNLOCK TABLES;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
 
--- Dump completed on 2026-01-07 17:09:47
+-- Dump completed on 2026-01-08 17:26:44
